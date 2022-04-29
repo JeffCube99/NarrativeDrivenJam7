1c1b899205c09dfcc3dddbf3d71a3a7cf5a86587
JeffCube99 <jeffcube99@gmail.com>
2022-04-28 22:47:30 -0700

    Added card animations for moving cards

--- Assets/_Scripts/CardMechanics/CardAppearanceController.cs
+++ Assets/_Scripts/CardMechanics/CardAppearanceController.cs
@@ -10,6 +10,12 @@ public class CardAppearanceController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI titleText;
     [SerializeField] private TextMeshProUGUI descriptionText;
     [SerializeField] private Image cardImage;
+    [SerializeField] private Animator cardAnimator;
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
+    [Range(1, 100f)] [SerializeField] private float maximumTiltVelocity = 1f;	// How fast you drag before you reach maximum card tilt
+    private Vector3 lastPosition;
+    private Vector3 dampedVelocity;
+    private Vector3 acceleration = Vector3.zero;
 
     private void Start()
     {
@@ -19,5 +25,20 @@ public class CardAppearanceController : MonoBehaviour
             descriptionText.text = card.description;
             cardImage.sprite = card.artwork;
         }
+
+        lastPosition = transform.position;
     }
+
+    private void Update()
+    {
+        Vector3 positionDelta = transform.position - lastPosition;
+        lastPosition = transform.position;
+        Vector3 targetVelocity = positionDelta / Time.deltaTime / maximumTiltVelocity;
+        dampedVelocity = Vector3.SmoothDamp(dampedVelocity, targetVelocity, ref acceleration, m_MovementSmoothing);
+        Debug.Log(dampedVelocity);
+        cardAnimator.SetFloat("x_velocity", dampedVelocity.x);
+        cardAnimator.SetFloat("y_velocity", dampedVelocity.y);
+    }
+
+
 }
